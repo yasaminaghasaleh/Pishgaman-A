3e89b44fe727434d90fbc6ed81ccebdf31eb8452
Arash <53490252+ThePythonist@users.noreply.github.com>
2022-07-14 18:38:53 +0430

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+std_code = int(input("Enter you code : "))
+
+print(name)
+print(age)
+print(std_code)
---
+++ 4.py
@@ -0,0 +1,3 @@
+birth = int(input("Enter your birth : "))
+age = 1401-birth
+print("Age :",age)
